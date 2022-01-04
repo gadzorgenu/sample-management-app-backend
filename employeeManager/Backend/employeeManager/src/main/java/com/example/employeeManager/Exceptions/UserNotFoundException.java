b652b3325be43df0d06ac15519aede30c348dae3
Georgina Adzorgenu <42786156+gadzorgenu@users.noreply.github.com>
2022-01-04 16:11:31 +0000

    Delete employeeManager directory

--- employeeManager/Backend/employeeManager/src/main/java/com/example/employeeManager/Exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeeManager.Exceptions;
-
-public class UserNotFoundException extends Exception {
-
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
